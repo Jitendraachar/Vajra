d7b1f7cdf843f926cb09ec91771eaf9ea071cf70
jitendra.y <jitendra.y@ISSLT560.inubesolutions.net>
2021-03-09 13:40:23 +0530

    intitiated chrome browser

--- Vajra/src/test/java/com/vajra/TestClass/Login_TestCase_001.java
+++ Vajra/src/test/java/com/vajra/TestClass/Login_TestCase_001.java
@@ -10,8 +10,10 @@ import com.vajra.TestClass.*;
 public class Login_TestCase_001 extends BaseClasss {
 	
 	@Test
-	public void Login_TestCase_001() {
+	public void Login_TestCase() {
+		//initiate the driver
 		LoginPageObject lpo= new LoginPageObject(driver);
+	
 		driver.get("URL");
 		lpo.EnterUserID();
 		lpo.EnterUserPass();
