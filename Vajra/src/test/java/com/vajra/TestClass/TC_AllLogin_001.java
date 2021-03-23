99fa5859d93a0cd1bcce2e9700919993aaba6a31
jitendra.y <jitendra.y@ISSLT560.inubesolutions.net>
2021-03-23 15:30:06 +0530

    changes for login using excel sheet

--- Vajra/src/test/java/com/vajra/TestClass/TC_AllLogin_001.java
+++ Vajra/src/test/java/com/vajra/TestClass/TC_AllLogin_001.java
@@ -1,13 +1,44 @@
 package com.vajra.TestClass;
 
 
+import java.io.IOException;
+
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.vajra.pageobjects.LoginPageObject;
 
 import junit.framework.Assert;
-
+import com.vajra.utilities.*;
 public class TC_AllLogin_001 extends BaseClasss {
+	
+	@DataProvider(name ="loginDetails") 
+	public static Object[][] login()throws IOException {
+		String path=System.getProperty("userdir")+"/src/test/java/com/vajra/TestData/Usernameandpassword.xlsx";
+		int rownum=XLUtilities.getRowCount(path, "Sale Login");
+		int cocount=XLUtilities.getCellCount(path, "Sale Login", 1);
+		String logindata[][]=new String [rownum][cocount];
+		for(int i=1;i<=rownum;i++)
+		{
+			for(int j=0;j<=cocount;j++)
+			{
+				logindata[i-1][j]=XLUtilities.getCellData( path,"Sale Login", i, j);
+			}
+		}
+		return logindata;
+		
+	}
+	
+	@Test(dataProvider="logindata")
+	public void LoginSales(String number,String User, String Password)throws InterruptedException,IOException
+	{
+		LoginPageObject lpo=new LoginPageObject(driver);
+		driver.get(SalesURL);
+		lpo.EnterUserID(User);
+		lpo.EnterUserPass(Password);
+		
+		
+	}
 
 	@Test(priority = 0)
 	public void Login_TestCase_SE() {
@@ -47,28 +78,20 @@ public class TC_AllLogin_001 extends BaseClasss {
 		Assert.assertEquals(driver.getTitle(), SalesTittle);
 	}
 	
-	@Test (priority = 3)
-	public void Login_TestCase_StoreHead() {
-		//initiate the driver
-		LoginPageObject lpo= new LoginPageObject(driver);
-
-		driver.get(SalesURL);
-
-		lpo.EnterUserID(StoreHeadID);
-		lpo.EnterUserPass(StoreHeadPass);
-		lpo.ClickOnLoginButton();
-		Assert.assertEquals(driver.getTitle(), SalesTittle);
-	}
-	
-	@Test(priority = 4)
-	public void Login_TestCase_OE() {
-		//initiate the driver
-		LoginPageObject lpo= new LoginPageObject(driver);
-		driver.get(OEURL);
-		lpo.EnterUserID(OEID);
-		lpo.EnterUserPass(OEPass);
-		lpo.ClickOnLoginButton();
-		Assert.assertEquals(driver.getTitle(), OETittle);
-	}
+	/*
+	 * @Test (priority = 3) public void Login_TestCase_StoreHead() { //initiate the
+	 * driver LoginPageObject lpo= new LoginPageObject(driver);
+	 * 
+	 * driver.get(SalesURL);
+	 * 
+	 * lpo.EnterUserID(StoreHeadID); lpo.EnterUserPass(StoreHeadPass);
+	 * lpo.ClickOnLoginButton(); Assert.assertEquals(driver.getTitle(),
+	 * SalesTittle); }
+	 * 
+	 * @Test(priority = 4) public void Login_TestCase_OE() { //initiate the driver
+	 * LoginPageObject lpo= new LoginPageObject(driver); driver.get(OEURL);
+	 * lpo.EnterUserID(OEID); lpo.EnterUserPass(OEPass); lpo.ClickOnLoginButton();
+	 * Assert.assertEquals(driver.getTitle(), OETittle); }
+	 */
 }
 
